3282d56dac92ed64dd7cdb1ce8554e0aae07d16c
Humza Butt <Humzab1711@hotmail.com>
2024-12-23 20:32:47 +0000

    Hashbrowser instead

--- server/server.js
+++ server/server.js
@@ -14,23 +14,23 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-// app.get('/manifest.json', (req, res) => {
-//     res.json({
-//       short_name: "CulinAIry",
-//       name: "CulinAIry App",
-//       icons: [],
-//       start_url: "/",
-//       display: "standalone",
-//       theme_color: "#ffffff",
-//       background_color: "#ffffff"
-//     });
-//   });
-  
+
 // Routes
 app.use('/api/recipes', recipeRoutes);
 app.use('/api/meal-planner', mealPlannerRoutes);
 
 // Start the server
+
+const path = require('path');
+
+// Serve React app
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Handle React routing, return all requests to React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
